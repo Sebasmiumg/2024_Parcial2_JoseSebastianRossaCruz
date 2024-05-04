ba83656e10ef0720591ae1cc5f957fb299f2764f
Sebasmiumg <jrossac@miumg.edu.gt>
2024-05-04 10:26:57 -0600

    ejercicio1 de serie 2

---
+++ main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+#include <string>
+
+namespace fs = std::filesystem;
+using namespace std;
+
+void crearArchivos(int cantidad) {
+    string nombreArchivo = "documento";
+    for (int i = 0; i < cantidad; ++i) {
+        string ruta;
+        if (i % 2 == 0) {
+            ruta = "C:/Users/Sebas/OneDrive/Escritorio/ARCHIVOS PAR";
+        } else {
+            ruta = "C:/Users/Sebas/OneDrive/Escritorio/ARCHIVOS IMPAR";
+        }
+        ofstream file(ruta + nombreArchivo + to_string(i + 1) + ".txt");
+        if (file.is_open()) {
+            agregarTexto(ruta + nombreArchivo + to_string(i + 1) + ".txt");
+            for (int j = 0; j < i + 1; ++j) {
+                file << "linea de texto" << endl;
+            }
+            file.close();
+            cout << "Archivo " << ruta << nombreArchivo << i + 1 << ".txt creado con éxito." << endl;
+        } else {
+            cout << "No se pudo abrir el archivo " << ruta << nombreArchivo << i + 1 << ".txt" << endl;
+        }
+    }
+}
+
+void mostrarMenu() {
+    cout << "Seleccione una opcion:" << endl;
+    cout << "1. Crear archivos" << endl;
+    cout << "2. Salir" << endl;
+}
+
+int main() {
+    int opcion;
+    do {
+        mostrarMenu();
+        cin >> opcion;
+        switch (opcion) {
+            case 1: {
+                int cantidad;
+                cout << "Ingrese la cantidad de archivos a crear: ";
+                cin >> cantidad;
+                crearArchivos(cantidad);
+                break;
+            }
+            case 2:
+                cout << "Saliendo del programa..." << endl;
+                break;
+            default:
+                cout << "Opción inválida. Por favor, seleccione una opción válida." << endl;
+                break;
+        }
+    } while (opcion != 2);
+    return 0;
+}
